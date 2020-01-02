0421efe13e809b8067a9cd1cd411cde7d7393d7b
oOTeamCocOo <46351483+oOTeamCocOo@users.noreply.github.com>
2020-01-02 12:15:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const Discord = require("discord.js");
 
+const client = new Discord.Client();
+
+var prefix = "*";
+
+client.on('ready', () => {
+    console.log('logged in as ' + client.user.tag)
+    client.user.setActivity('la sécurité de ton serveur !',{type: 'WATCHING'})
+});
+
+client.login(process.env.TOKEN)
