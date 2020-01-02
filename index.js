39649ca3ea5a921f63255b1b787704b3211e4c51
oOTeamCocOo <46351483+oOTeamCocOo@users.noreply.github.com>
2020-01-02 13:12:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,56 +11,7 @@ client.on('ready', () => {
 
 client.login(process.env.TOKEN)
 
-client.on("message" , message => {
-    if(message.content === prefix + "help") {
-        var info_embed = new Discord.RichEmbed()
-        .setColor('e70647')
-        .setTitle('```Voici la liste de commande```\n\n-------------------------------------------')
-        .setDescription('*help: liste de commande\n\n*blacklist: avoir la liste noire\n\n*invite: avoire lien invitation bot\n\n*info: info du jour\n\n\n-------------------------------------------\n\n [ADMINISTRATION]\n\n*securise: activer mode sécurité\n\n*clear [NOMBRE] effacer un lot de message\n\n\n-------------------------------------------\n\n [OPTIONNEL]\n\n *star1502: Savoir l histoire de ce YouTubeur\n\n\n-------------------------------------------')
-        .setTimestamp()
-        .setFooter('Edité par oOTeamCocOo')
-        message.channel.sendMessage(info_embed)
-        message.author.sendMessage('```Tu as demander le help !\nN hesite pas à m ajouter a ton serveur ! :``` https://discordapp.com/api/oauth2/authorize?client_id=569192657536942162&permissions=0&scope=bot ')
-        console.log("Un utilisateur à demander help")
-    }
-});
-
-
-client.on("message" , message => {
-    if(message.content === "*blacklist"){
-        message.reply(" ```Voici les gens possédant des idées noire:``` @Chrisopeer Davies et @Jessica Davies");
-        console.log("Blacklist demandé")
-    }
-});
-
-client.on("message" , message => {
-    if(message.content === "*securise"){
-        message.reply("```Le mode securité est bientôt disponible, en attendant, fais : *aie```");
-        console.log("Mode securité demander")
-    }
-});
-
-client.on("message" , message => {
-    if(message.content === "Wsh"){
-        message.reply("```Je n'aime pas la façons dont tu parle ! [Attention]```");
-        console.log("Un utilisateur à dis WESH, je n'aime pas ça")
-    }
-});
-
-client.on("message" , message => {
-    if(message.content === "Tg"){
-        message.reply("```[WARNING] On ne parle pas comme ça à ses camarades DISCORD !```");
-        console.log("Un utilisateur à dis TAGEULE, je n'aime pas ça")
-    }
-});
-
-client.on("message" , message => {
-    if(message.content === "Slt"){
-        message.reply("```Bonjour, mon créateur, c'est oOTeamCocOo, je suis déstiné à apporter la protection de tous !```");
-        message.author.sendMessage('```N hesite pas à m ajouter a ton serveur ! :``` https://discordapp.com/api/oauth2/authorize?client_id=569192657536942162&permissions=0&scope=bot ')
-        console.log("un utilisateur à dis salut")
-    }
-});
+;
 
 client.on("message" , message => {
     if(message.content === "Hey"){
@@ -90,13 +41,3 @@ client.on("message" , message => {
 
 
 
-client.on('message' , message => {
-    if(message.content === prefix +'reglement') {
-        var reglement_embed = new Discord.RichEmbed()
-        .setColor('f3f800')
-        .setTitle('```REGLEMENT```')
-        .setDescription('MON PREFIX EST "*" \n\n1) Pas de everyone sur notre serveur.\n\n 2) Les insultes sont interdites et seront sanctionnés.\n\n 3) Pas de pubs pour vos serveurs discord.\n\n 4) Ajouter le not avec la commande *invite.\n\n 5) Les liens inappropriés comme des screamers et des sites pornographiques sont automatiquement supprimés.\n\n 6) Ne pas spammer/flood, utiliser trop de caractères spéciaux ou encore écrire de longs messages en MAJUSCULES.\n\n 7) Respectez tous les membres de façon égale et soyez gentils les uns envers les autres. Si vous avez des problèmes avec quelqu un ou que vous vous sentez harcelé, contactez un membre du staff.\n\n\n-------------------------------------------')
-        .setFooter('réglement 2019')
-        .setTimestamp()
-        message.channel.sendMessage(reglement_embed)
-        console.log('le reglement à été d
